0500d4e79e45477b181882595228023f94a5ca8f
Xiao Hu <huxiao198911@gmail.com>
2021-09-29 11:37:16 +0200

    database for design, illustration and their subpages

---
+++ js/database-fas-illu.js
@@ -0,0 +1,325 @@
+const workTool = ['iPad Pro + Procreate', 'Macbook + Adobe Photoshop', 'PC + Adobe Photoshop'];
+const imgDirHead = ['../../images/fashion/design/', '../../images/fashion/garment/', '../../images/illustration/gufeng/', '../../images/illustration/life-story/', '../../images/illustration/books/'];
+// ##### Fashion - Designs works #####
+const designWorks = [];
+let designWorkArticle = {};
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Black-red Cheongsam with miniskirt',
+    imgDir: imgDirHead[0] + 'black-red-mini-skirt-cheongsam.jpg',
+    creativeDuration: 'Date: May 16-22 2021',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Dark red coat with yellow-purple strip',
+    imgDir: imgDirHead[0] + 'dark-red-yellow-strip-coat.jpg',
+    creativeDuration: 'Date: December 12-14 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Ruffled prom dress in purple',
+    imgDir: imgDirHead[0] + 'purple-ruffle-prom-dress.jpg',
+    creativeDuration: 'Date: August 29-30 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Sisterhood outfit in yellow',
+    imgDir: imgDirHead[0] + 'yellow-black-white-suit-dress.jpg',
+    creativeDuration: 'Date: May 22-24 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Purple one piece dress with Chinese style',
+    imgDir: imgDirHead[0] + 'purple-chinese-one-piece-dress.jpg',
+    creativeDuration: 'Date: May 12-13 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: pink-plastic-transparent-raincoat-umbrella',
+    imgDir: imgDirHead[0] + 'Pink lady with raincoat.jpg',
+    creativeDuration: 'Date: May 01-03 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Lady in yellow clown suit',
+    imgDir: imgDirHead[0] + 'yellow-clown-suit-buff-sleeve.jpg',
+    creativeDuration: 'Date: April 18-29 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Dance in blue dress',
+    imgDir: imgDirHead[0] + 'blue-dress-fluffy-sleevee.jpg',
+    creativeDuration: 'Date: April 18-21 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+designWorkArticle = {
+    title: 'Title: Green black princess dress',
+    imgDir: imgDirHead[0] + 'green-black-princess-dress.jpg',
+    creativeDuration: 'Date: April 16 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+designWorks.push(designWorkArticle);
+
+//console.log(designWorks);
+
+// ##### Illustration - Gufeng - Chinese-style works #####
+const gufengWorks = [];
+let gufengWorkArticle = {};
+gufengWorks.push(gufengWorkArticle);
+
+gufengWorkArticle = {
+    title: 'Title: Moonlight',
+    imgDir: imgDirHead[2] + 'gufeng-red-dress.jpg',
+    creativeDuration: 'Date: March 18 2021',
+    tool: 'Tool: ' + workTool[0]
+}
+gufengWorks.push(gufengWorkArticle);
+
+gufengWorkArticle = {
+    title: 'Title: Girl in red Chinese dress',
+    imgDir: imgDirHead[2] + 'gufeng-moonlight.jpg',
+    creativeDuration: 'Date: May 06 2021',
+    tool: 'Tool: ' + workTool[0]
+}
+gufengWorks.push(gufengWorkArticle);
+
+gufengWorkArticle = {
+    title: 'Title: Blue series - Spring',
+    imgDir: imgDirHead[2] + 'gufeng-blue-series-spring.jpg',
+    creativeDuration: 'Date: March 17-21 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+gufengWorks.push(gufengWorkArticle);
+
+gufengWorkArticle = {
+    title: 'Title: Blue series - Dance',
+    imgDir: imgDirHead[2] + 'gufeng-blue-series-dance.jpg',
+    creativeDuration: 'Date: March 08 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+gufengWorks.push(gufengWorkArticle);
+
+gufengWorkArticle = {
+    title: 'Title: Blue series - Fan',
+    imgDir: imgDirHead[2] + 'gufeng-blue-series-fan.jpg',
+    creativeDuration: 'Date: March 02 2020',
+    tool: 'Tool: ' + workTool[0]
+}
+gufengWorks.push(gufengWorkArticle);
+
+
+// ##### Illustration - Life story  works #####
+const lifeStoryWorks = [];
+let lifeStoryWorkArticle = {};
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Party is not my thing',
+    imgDir: imgDirHead[3] + 'party-lonely.jpg',
+    creativeDuration: 'Date: June 10 2021',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>People in Europe are big fan of parties. To be honest, I am not really into these type of parties - just drinking and talking. I feel awkward and lonely when I was surrounded by strangers. I don\'t know what shall I talk with them...</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Kick Boxing',
+    imgDir: imgDirHead[3] + 'sports-kick-boxing.jpg',
+    creativeDuration: 'Date: June 10 2021',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>I was doing kick-boxing with other 2 girls. We are not tall, so coach asked us to stand on the step and then practice the punches with him one by one. I told my friend, with the same height, she can first grab coach \'s neck， and kick him hardly direct to his stomach. Coach: \'why you are such a violent girl?\'</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Being the Queen',
+    imgDir: imgDirHead[3] + 'queen.jpg',
+    creativeDuration: 'Date: June 10 2021',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>Well, sometimes I just this imagination- I am the Queen. I know, it is a bit crazy but funny I guess.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Urban trail city run',
+    imgDir: imgDirHead[3] + 'city-run.jpg',
+    creativeDuration: 'Date: July 24 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>I joined the urban trail running  event to explore the city. When I was running through a big shopping mall with others, a tall guy suddenly yelled at me:</p><p>\'Lianru, go go go!\'</p><p>I glanced at him and puzzled :\'Who is he? Do I know him\'.</p><p>Later on, I saw my name was on the tag from my clothes . Properly he saw my name from there.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Big face',
+    imgDir: imgDirHead[3] + 'big-face.jpg',
+    creativeDuration: 'Date: July 17 2018',
+    tool: 'Tool: ' + workTool[1],
+    story: '<h2>Story: </h2><p>We are having a boat trip in Amsterdam. A friend sat next to me was taking pictures of people on the boat. Suddenly, he turned his camera towards me and yelling at me:</p><p>\'Wow, your face is so big!\'</p><p>\'Of course, my face looks big, because your camera is very close to me.\' I said to him angrily.</p><p>In my mind, I am thinking \'How dare you tell a girl her face is big? Are you crazy? lol\'</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Guy VS lady reacts after my hair cut',
+    imgDir: imgDirHead[3] + 'hair-cut.jpg',
+    creativeDuration: 'Date: June 22 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>After I cut my hair a bit short, the comments I get from my female colleagues are always nice and make me happy:</p><p>\'You look chic with this hair style\'</p><p>\'I like your new haircut\'</p><p>\'You looks cute in the short hair style\'</p><p>And guys\' feedback ... </p><p>\'I like girl with longer hair\'</p><p>\'Oh, you cut your hair short\' with a sad face</p><p></p><p>\'Oh, you did not listen to me, you still cut your hair short\'</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: I want food!',
+    imgDir: imgDirHead[3] + 'sports-food.jpg',
+    creativeDuration: 'Date: May 01 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>I am doing one to one training with my sports coach. He is eating his food when he is watching me to do the ab exercise. Seriously, I also want to eat food, aaaa.. Such a torture.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Computer crushes',
+    imgDir: imgDirHead[3] + 'collapse.jpg',
+    creativeDuration: 'Date: April 11 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>There are always some moments, when my work almost done, my laptop suddenly crushed and my work was not saved. I have to start from scratch again.This makes me so frustrated.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Computer crushes',
+    imgDir: imgDirHead[3] + 'collapse.jpg',
+    creativeDuration: 'Date: April 11 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>There are always some moments, when my work almost done, my laptop suddenly crushed and my work was not saved. I have to start from scratch again.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Hug',
+    imgDir: imgDirHead[3] + 'hug.jpg',
+    creativeDuration: 'Date: March 26 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>Hug is a nice way of comforting people.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Friday',
+    imgDir: imgDirHead[3] + 'friday.jpg',
+    creativeDuration: 'Date: March 26 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>When Friday comes, I have no mood to do work, just want to go home as early as possible to start my weekend!</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Restart',
+    imgDir: imgDirHead[3] + 'restart.jpg',
+    creativeDuration: 'Date: March 22 2018',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>Just drew this.. that is it.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Lost My Way',
+    imgDir: imgDirHead[3] + 'lost-my-way.jpg',
+    creativeDuration: 'Date: September 6 2013',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>Feeling lost in my life, I don\'t know which direction I shall continue in my life.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Happy Chinese New Year!',
+    imgDir: imgDirHead[3] + 'chinese-new-year.jpg',
+    creativeDuration: 'Date: February 08 2013',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>2013 was the first Chinese new year I had in the Netherlands without family around.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: I am not in the mood!',
+    imgDir: imgDirHead[3] + 'crazy.jpg',
+    creativeDuration: 'Date: February 04 2013',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>From time to time, there always a period that I am feeling so down without any reason. And I don\'t want to do anything except hiding myself in a corner.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title:  Christmas gift',
+    imgDir: imgDirHead[3] + 'christmas.jpg',
+    creativeDuration: 'Date: December 21 2012',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>When I was a kid, I always wished to receive a nice gift from Santa during Christmas. However, it had never happened, because it was not a thing in China. So my parents would not prepare a gift to me as Santa.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title:  Happy Birthday to myself',
+    imgDir: imgDirHead[3] + 'birthday-cake.jpg',
+    creativeDuration: 'Date: March 23 2009',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>I don\'t remember when I drew this picture for myself. I am not a big fan of birthday celebration. So I barely celebrate my own birthday after 10 years old. But it is still nice to have a nice cake on birthday.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+lifeStoryWorkArticle = {
+    title: 'Title: Lucky Me',
+    imgDir: imgDirHead[3] + 'lunch-break-nap.jpg',
+    creativeDuration: 'Date: March 23 2009',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>A short nap after lunch was a much do thing, when I was working in China.</p>'
+}
+lifeStoryWorks.push(lifeStoryWorkArticle);
+
+//console.log(lifeStoryWorks);
+
+
+// ##### Illustration - Books works #####
+const booksWorks = [];
+let booksWorkArticle = {};
+booksWorks.push(booksWorkArticle);
+
+booksWorkArticle = {
+    title: 'Title: Group Benefits from Genomic Instability',
+    creativeDuration: 'Date: June 2020',
+    tool: 'Tool: ' + workTool[0],
+    story: '<h2>Story: </h2><p>Zheren is one of my best friends. He is a PHD of microbiology. He invited me to draw the cover and bookmark for his PHD book. He is from China and he did his PHD at Leiden university in the Netherlands, so I made some Chinese and Dutch houses to represent the mix culture he had experience. At the back cover, it demos a liftcycle of the bacteria he researched during his PHD.Now he is doing his post PHD at Oxford university. I am very proud of him.</p>'
+}
+booksWorks.push(booksWorkArticle);
+
+booksWorkArticle = {
+    title: 'Title:  Who Would I Be If I Weren\'t Me?',
+    creativeDuration: 'Date:  December 2014-January 2015',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>This book is part of one of sub-projects of ‘Realising Potential’ research for children development at Oxford University. I did the drawings for this book.</p>'
+}
+booksWorks.push(booksWorkArticle);
+
+booksWorkArticle = {
+    title: 'Title: Why the Moon Has Crater',
+    creativeDuration: 'Date: Nov 2012 -Jan 2013',
+    tool: 'Tool: ' + workTool[2],
+    story: '<h2>Story: </h2><p>This book is a wedding gift Mark L.Takefman gave to his daughter. He told this story when his daughter was a little girl. It is about how a dragon girl makes the moon having craters.  He invited me to do the illustration parts for this book. Such a lovely gift.</p>'
+}
+booksWorks.push(booksWorkArticle);
